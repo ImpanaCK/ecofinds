4f5de8203e589f295f685139f70cf2e8aa4fdedf
Impana <160823421+ImpanaCK@users.noreply.github.com>
2025-09-06 16:56:08 +0530

    correct mistakes in urls.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to EcoFinds!</h1>")
