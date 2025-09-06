b865bf47617314c30d426a6b0c383e7e0752ffde
Impana <160823421+ImpanaCK@users.noreply.github.com>
2025-09-06 15:01:35 +0530

    completed crud functionalities

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]  # still keep username optional
+
+
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()  # Use your custom user model
+
+class Product(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
